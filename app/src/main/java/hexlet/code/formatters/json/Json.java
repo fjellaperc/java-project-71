4cb946f7c83a13697b660707d6bca9602625bca3
fjellaperc <fjellaperc@gmail.com>
2023-09-28 18:33:06 +0300

    add whitespaces another one

--- app/src/main/java/hexlet/code/formatters/json/Json.java
+++ app/src/main/java/hexlet/code/formatters/json/Json.java
@@ -18,15 +18,15 @@ public class Json {
         LinkedHashMap<String, Object> result = new LinkedHashMap<>();
         for (String key : keys) {
             if (parsMap1.containsKey(key) && !parsMap2.containsKey(key)) { // В первой есть во 2 нет
-                result.put(" - " + key + ":", parsMap1.get(key));
+                result.put("   - " + key + ":", parsMap1.get(key));
             } else if (!parsMap1.containsKey(key) && parsMap2.containsKey(key)) { // Во второй есть в 1 нет
-                result.put(" + " + key + ":", parsMap2.get(key));
+                result.put("   + " + key + ":", parsMap2.get(key));
             } else {
                 if (Objects.equals(parsMap1.get(key), parsMap2.get(key))) { //Ключи есть и значения равны
-                    result.put("  " + key + ":", parsMap1.get(key));
+                    result.put("    " + key + ":", parsMap1.get(key));
                 } else { //Значения не равны
-                    result.put(" - " + key + ":", parsMap1.get(key));
-                    result.put(" + " + key + ":", parsMap2.get(key));
+                    result.put("   - " + key + ":", parsMap1.get(key));
+                    result.put("   + " + key + ":", parsMap2.get(key));
                 }
             }
         }
