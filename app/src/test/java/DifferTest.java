021c4893c0bbc9677fcd4d40f29f2f855fcef610
fjellaperc <fjellaperc@gmail.com>
2023-10-22 16:38:43 +0300

    fix tests YAML

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -13,8 +13,10 @@ class DifferTest {
     private File stylishTest;
     private File plainTest;
     private File emptyFile1;
-    private File dataStructure1;
-    private File dataStructure2;
+    private File dataStructureJson1;
+    private File dataStructureJson2;
+    private File dataStructureYAML1;
+    private File dataStructureYAML2;
     private String stylish = "stylish";
     private String plain = "plain";
 
@@ -25,8 +27,10 @@ class DifferTest {
         emptyFile1 = new File("./src/test/resources/emptyfile1.json");
         stylishTest = new File("./src/test/resources/stylishTest");
         plainTest = new File("./src/test/resources/plainTest");
-        dataStructure1 = new File("./src/test/resources/DataStructure1.json");
-        dataStructure2 = new File("./src/test/resources/DataStructure2.json");
+        dataStructureJson1 = new File("./src/test/resources/DataStructure1.json");
+        dataStructureJson2 = new File("./src/test/resources/DataStructure2.json");
+        dataStructureYAML1 = new File("./src/test/resources/DataStructure1.yaml");
+        dataStructureYAML2 = new File("./src/test/resources/DataStructure2.yml");
     }
 
     public static String getDataFromFile(File file) throws IOException {
@@ -40,16 +44,32 @@ class DifferTest {
         Assertions.assertEquals(expected1, actual1);
     }
     @Test
-    public void isStylishTest() throws Exception {
+    public void isStylishJSONTest() throws Exception {
         String expected2 = getDataFromFile(stylishTest);
-        String actual2 = Differ.generate(dataStructure1.getAbsolutePath(), dataStructure2.getAbsolutePath(), stylish);
+        String actual2 = Differ.generate(dataStructureJson1.getAbsolutePath(), dataStructureJson2.getAbsolutePath(), stylish);
         Assertions.assertEquals(expected2, actual2);
     }
     @Test
-    public void isPlainTest() throws Exception {
+    public void isPlainJSONTest() throws Exception {
         String expected2 = getDataFromFile(plainTest);
-        String actual2 = Differ.generate(dataStructure1.getAbsolutePath(), dataStructure2.getAbsolutePath(), plain);
+        String actual2 = Differ.generate(dataStructureJson1.getAbsolutePath(), dataStructureJson2.getAbsolutePath(), plain);
         Assertions.assertEquals(expected2, actual2);
     }
+
+    @Test
+    public void isStylishYAMLTest() throws Exception {
+        String expected2 = getDataFromFile(stylishTest);
+        String actual2 = Differ.generate(dataStructureYAML1.getAbsolutePath(), dataStructureYAML2.getAbsolutePath(),
+                stylish);
+        Assertions.assertEquals(expected2, actual2);
+    }
+    @Test
+    public void isPlainYAMLTest() throws Exception {
+        String expected2 = getDataFromFile(plainTest);
+        String actual2 = Differ.generate(dataStructureYAML1.getAbsolutePath(), dataStructureYAML2.getAbsolutePath(),
+                plain);
+        Assertions.assertEquals(expected2, actual2);
+    }
+
 }
 
